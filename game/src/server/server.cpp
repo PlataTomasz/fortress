a9cb99142c1e580f69a55eaafc2c39d00d934767
Timux_ <thetimux@gmail.com>
2022-10-24 16:56:57 +0200

    Stubs for client and server; Updated build script

--- game/src/server/server.cpp
+++ game/src/server/server.cpp
@@ -1,1 +1,19 @@
- 
+#include "server.h"
+
+#include <godot_cpp/classes/ref.hpp>
+#include <godot_cpp/classes/multiplayer_api.hpp>
+#include <godot_cpp/classes/scene_tree.hpp>
+
+using namespace godot;
+
+void Server::_ready()
+{
+    this->networkPeer = memnew(ENetMultiplayerPeer);
+
+    this->networkPeer->create_server(7654, 32, 4);
+
+    Ref<MultiplayerAPI> mAPI = memnew(MultiplayerAPI);
+    mAPI->set_multiplayer_peer(networkPeer);
+
+    this->get_tree()->set_multiplayer(mAPI);
+}
