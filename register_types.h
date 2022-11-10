7e76ea4ace790ab3dcdd94181984136ce38fa61b
Timux_ <thetimux@gmail.com>
2022-11-10 13:41:38 +0100

    Migration to engine codebase solution instead of GDExtension

---
+++ register_types.h
@@ -0,0 +1,9 @@
+#ifndef ARTIFACT_SEEKERS_CLIENT_INCLUDED
+#define ARTIFACT_SEEKERS_CLIENT_INCLUDED
+
+#include "modules/register_module_types.h"
+
+void initialize_artifact_seekers_module(ModuleInitializationLevel p_level);
+void uninitialize_artifact_seekers_module(ModuleInitializationLevel p_level);
+
+#endif // ARTIFACT_SEEKERS_CLIENT_INCLUDED
