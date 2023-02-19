e4290d2d9a0aa17aff2e362891d42d4024b405b9
Timux_ <thetimux@gmail.com>
2023-02-19 15:46:08 +0100

    Added helper methods for easier access of core game objects

--- src/gdextension_helper.hpp
+++ src/gdextension_helper.hpp
@@ -6,6 +6,20 @@
 
 #define DISABLE_IN_EDITOR() if(Engine::get_singleton()->is_editor_hint()) return;
 
+class Client;
+class Game;
+
+static Client* get_client()
+{
+    return get_scene_tree()->get_root()->get_node<Client>("Client");
+}
+
+static Game* get_game()
+{
+    return get_scene_tree()->get_root()->get_node<Game>("Client/Game");
+}
+
+
 static SceneTree* get_scene_tree()
 {
     SceneTree* sceneTreeSingleton = (SceneTree*)Engine::get_singleton()->get_main_loop();
