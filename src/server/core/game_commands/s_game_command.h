17ab6ca0f275bf000173c012aa6d8d8691dc5d7b
Tomasz Plata <tomaszplata10@gmail.com>
2023-10-01 15:42:56 +0200

    Stub for server

---
+++ src/server/core/game_commands/s_game_command.h
@@ -0,0 +1,17 @@
+#if !defined(S_GAME_COMMAND_INCLUDE)
+#define S_GAME_COMMAND_INCLUDE
+
+/**
+ * Object 
+*/
+class SGameCommand
+{
+private:
+
+protected:
+
+public:
+
+};
+
+#endif // S_GAME_COMMAND_INCLUDE
