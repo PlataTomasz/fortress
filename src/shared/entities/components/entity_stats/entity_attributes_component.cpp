5be2eaf34747534e040821bd72fe1b02358280ea
TTimux <thetimux@gmail.com>
2024-05-13 11:35:19 +0200

    Modified Entity Attributes component, to expose stat objects

--- src/shared/entities/components/entity_stats/entity_attributes_component.cpp
+++ src/shared/entities/components/entity_stats/entity_attributes_component.cpp
@@ -1,19 +1,11 @@
 #include "entity_attributes_component.h"
 
-Ref<FloatAttribute> EntityAttributesComponent::get_current_health() {
-    return current_health;
+Ref<FloatAttribute> EntityAttributesComponent::get_health() {
+    return health;
 }
 
-void EntityAttributesComponent::set_current_health(Ref<FloatAttribute> attribute) {
-    current_health = attribute;
-}
-
-Ref<FloatAttribute> EntityAttributesComponent::get_max_health() {
-    return max_health;
-}
-
-void EntityAttributesComponent::set_max_health(Ref<FloatAttribute> attribute) {
-    max_health = attribute;
+void EntityAttributesComponent::set_health(Ref<FloatAttribute> attribute) {
+    health = attribute;
 }
 
 Ref<FloatAttribute> EntityAttributesComponent::get_global_defense() {
@@ -62,4 +54,35 @@ Ref<FloatAttribute> EntityAttributesComponent::get_movement_speed() {
 
 void EntityAttributesComponent::set_movement_speed(Ref<FloatAttribute> attribute) {
     movement_speed = attribute;
+}
+
+
+void EntityAttributesComponent::_bind_methods() {
+    ::ClassDB::bind_method(D_METHOD("get_health"), &EntityAttributesComponent::get_health);
+    ::ClassDB::bind_method(D_METHOD("set_health"), &EntityAttributesComponent::set_health);
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "health", PROPERTY_HINT_RESOURCE_TYPE, "CappedResourceAttribute"), "set_health", "get_health");
+
+    ::ClassDB::bind_method(D_METHOD("get_global_defense"), &EntityAttributesComponent::get_global_defense);
+    ::ClassDB::bind_method(D_METHOD("set_global_defense"), &EntityAttributesComponent::set_global_defense);
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "global_defense", PROPERTY_HINT_RESOURCE_TYPE, "FloatAttribute"), "set_global_defense", "get_global_defense");
+
+    ::ClassDB::bind_method(D_METHOD("get_attack_damage"), &EntityAttributesComponent::get_attack_damage);
+    ::ClassDB::bind_method(D_METHOD("set_attack_damage"), &EntityAttributesComponent::set_attack_damage);
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "attack_damage", PROPERTY_HINT_RESOURCE_TYPE, "FloatAttribute"), "set_attack_damage", "get_attack_damage");
+
+    ::ClassDB::bind_method(D_METHOD("get_attack_speed"), &EntityAttributesComponent::get_attack_speed);
+    ::ClassDB::bind_method(D_METHOD("set_attack_speed"), &EntityAttributesComponent::set_attack_speed);
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "attack_speed", PROPERTY_HINT_RESOURCE_TYPE, "FloatAttribute"), "set_attack_speed", "get_attack_speed");
+
+    ::ClassDB::bind_method(D_METHOD("get_magic_power"), &EntityAttributesComponent::get_magic_power);
+    ::ClassDB::bind_method(D_METHOD("set_magic_power"), &EntityAttributesComponent::set_magic_power);
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "magic_power", PROPERTY_HINT_RESOURCE_TYPE, "FloatAttribute"), "set_magic_power", "get_magic_power");
+
+    ::ClassDB::bind_method(D_METHOD("get_cooldown_reduction"), &EntityAttributesComponent::get_cooldown_reduction);
+    ::ClassDB::bind_method(D_METHOD("set_cooldown_reduction"), &EntityAttributesComponent::set_cooldown_reduction);
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "cooldown_reduction", PROPERTY_HINT_RESOURCE_TYPE, "FloatAttribute"), "set_cooldown_reduction", "get_cooldown_reduction");
+
+    ::ClassDB::bind_method(D_METHOD("get_movement_speed"), &EntityAttributesComponent::get_movement_speed);
+    ::ClassDB::bind_method(D_METHOD("set_movement_speed"), &EntityAttributesComponent::set_movement_speed);
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "movement_speed", PROPERTY_HINT_RESOURCE_TYPE, "FloatAttribute"), "set_movement_speed", "get_movement_speed");
 }
--- src/shared/entities/components/entity_stats/entity_attributes_component.h
+++ src/shared/entities/components/entity_stats/entity_attributes_component.h
@@ -10,23 +10,19 @@ GDCLASS(EntityAttributesComponent, Node);
 private:
     
 protected:
-    static void _bind_methods(){};
+    static void _bind_methods();
 public:
-    Ref<FloatAttribute> current_health;
-    Ref<FloatAttribute> max_health;
+    Ref<CappedResourceAttribute> health;
     //Reduces damage no matter damage type
     Ref<FloatAttribute> global_defense;
     Ref<FloatAttribute> attack_damage;
     Ref<FloatAttribute> attack_speed;
     Ref<FloatAttribute> magic_power;
     Ref<FloatAttribute> cooldown_reduction;
-    Ref<FloatAttribute> max_cooldown_reducation;
     Ref<FloatAttribute> movement_speed;
 
-    Ref<FloatAttribute> get_current_health();
-    void set_current_health(Ref<FloatAttribute> attribute);
-    Ref<FloatAttribute> get_max_health();
-    void set_max_health(Ref<FloatAttribute> attribute);    
+    Ref<FloatAttribute> get_health();
+    void set_health(Ref<FloatAttribute> attribute);
     Ref<FloatAttribute> get_global_defense();
     void set_global_defense(Ref<FloatAttribute> attribute);
     Ref<FloatAttribute> get_attack_damage();
@@ -35,14 +31,8 @@ public:
     void set_attack_speed(Ref<FloatAttribute> attribute);
     Ref<FloatAttribute> get_magic_power();
     void set_magic_power(Ref<FloatAttribute> attribute);
-    Ref<FloatAttribute> get_damage_rampup();
-    void set_damage_rampup(Ref<FloatAttribute> attribute);
-    Ref<FloatAttribute> get_max_damage_rampup();
-    void set_max_damage_rampup(Ref<FloatAttribute> attribute);
     Ref<FloatAttribute> get_cooldown_reduction();
     void set_cooldown_reduction(Ref<FloatAttribute> attribute);
-    Ref<FloatAttribute> get_max_cooldown_reduction();
-    void set_max_cooldown_reduction(Ref<FloatAttribute> attribute);
     Ref<FloatAttribute> get_movement_speed();
     void set_movement_speed(Ref<FloatAttribute> attribute);
 
