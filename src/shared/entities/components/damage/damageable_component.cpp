c49aec427c8ba48b0a3bd94fac20bc4a4bcb8447
TTimux <thetimux@gmail.com>
2024-05-13 11:56:57 +0200

    Damageable component needs further refactor due to changes to EntityAttributesComponent

--- src/shared/entities/components/damage/damageable_component.cpp
+++ src/shared/entities/components/damage/damageable_component.cpp
@@ -32,7 +32,7 @@ void DamageableComponent::take_damage(float damage_value, Node *inflictor, Node 
     EntityAttributesComponent *attributes_component = ComponentManager::get_component<EntityAttributesComponent>(get_parent());
 	float global_defense_val = attributes_component->get_global_defense()->get_current();
 
-	float health_value = attributes_component->get_current_health()->get_current();
+	float health_value = attributes_component->get_health()->get_current();
 
 	//Reduce incoming damage with global defense - Damage reduction equation
 	float damage_multiplier = global_defense_val / (100 + global_defense_val);
@@ -41,7 +41,7 @@ void DamageableComponent::take_damage(float damage_value, Node *inflictor, Node 
 	health_modifier->set_type(FloatValueModifier::Type::FLAT_ADD);
 	health_modifier->set_value(-health_modifier->get_value());
 
-	bool was_lethal = attributes_component->get_current_health()->get_current() <= 0;
+	bool was_lethal = attributes_component->get_health()->get_current() <= 0;
 
 	emit_signal("damage_taken", damage_value, inflictor, caused_by, was_lethal);
 
@@ -53,7 +53,7 @@ void DamageableComponent::take_damage(float damage_value, Node *inflictor, Node 
 void DamageableComponent::heal(float value) {
     // FIXME: Change this, to get component from entity itself
     EntityAttributesComponent *attributes_component = ComponentManager::get_component<EntityAttributesComponent>(get_parent());
-    Ref<CappedResourceAttribute> health_attribute = attributes_component->get_max_health();
+    Ref<CappedResourceAttribute> health_attribute = attributes_component->get_health();
 
     float max_health = health_attribute->get_current();
     float current_health = health_attribute->get_current();
