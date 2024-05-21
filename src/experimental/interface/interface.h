c58ac4245f0ddd675706075567cff24fe905c406
TTimux <thetimux@gmail.com>
2024-05-21 18:23:20 +0200

    Introduced Interface type; Interface deriving types can be used to create a callable with callable_mp

---
+++ src/experimental/interface/interface.h
@@ -0,0 +1,13 @@
+#if !defined(INTERFACE_INCLUDED)
+#define INTERFACE_INCLUDED
+
+#include <core/object/object.h>
+#include <core/object/class_db.h>
+
+class Interface : public Object {
+GDCLASS(Interface, Object);
+
+
+};
+
+#endif // INTERFACE_INCLUDED
