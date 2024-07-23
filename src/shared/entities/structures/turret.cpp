b364c68847899ef0355ad39e390309dcef53ffc4
TTimux <thetimux@gmail.com>
2024-07-23 13:17:13 +0200

    Trying to implement methods responsible for finding new target and comparing targets based on their priority and distance from turret

--- src/shared/entities/structures/turret.cpp
+++ src/shared/entities/structures/turret.cpp
@@ -15,6 +15,58 @@ Entity *Turret::find_new_target() {
     4. Back to step 2
     5. Return potential target
     */
+    Vector<Entity *> entities_in_area; // TODO: Currently null
+
+    Entity *closest_entity = get_closest_entity(entities_in_area);
+
+    Entity *new_target = nullptr;
+    int new_target_aggro_priority = 0;
+    for(Entity *potential_target : entities_in_area) {
+        if(new_target_aggro_priority > ) {
+            
+        }
+    }
+
+    return new_target;
+}
+
+bool Turret::are_entities_aggro_priority_equal() {
+    //if(first_aggro_)
+    return false;
+}
+
+Entity *Turret::get_closest_entity(const Vector<Entity *> &potential_closest_entities) {
+	ERR_FAIL_COND_V(!(potential_closest_entities.size() > 0), nullptr);
+
+    struct ClosestEntityData {
+        Entity *entity = nullptr;
+        float squared_distance_to = 0;
+    } closest_entity_data;
+
+	closest_entity_data.entity = potential_closest_entities[0];
+    closest_entity_data.squared_distance_to = closest_entity_data.entity->get_position_2d().distance_squared_to(this->get_position_2d());
+
+    // TODO: Create GameCordinates class to store position of entity with possible ignore of y axis, to make easier messing with coordinates
+
+	for (int potential_closest_index = 0; potential_closest_index < potential_closest_entities.size(); potential_closest_index++) {
+        Entity *potential_closest_entity = potential_closest_entities[potential_closest_index];
+        float potential_closest_entity_distance = potential_closest_entity->get_position_2d().distance_squared_to(this->get_position_2d());
+
+		if (potential_closest_entity_distance < closest_entity_data.squared_distance_to) {
+            closest_entity_data.entity = potential_closest_entity;
+            closest_entity_data.squared_distance_to = potential_closest_entity_distance;
+		}
+	}
+
+	return closest_entity_data.entity;
+}
+
+int get_higher_aggro_priority(int first_aggro_priority, int second_aggro_priority) {
+    if(first_aggro_priority > second_aggro_priority) {
+        return first_aggro_priority;
+    } else if(second_aggro_priority >= first_aggro_priority) {
+        return second_aggro_priority;
+    }
 }
 
 int Turret::get_aggro_priority_for_entity(Entity *entity) {
