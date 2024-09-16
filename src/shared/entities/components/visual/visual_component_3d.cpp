4cd5fe1cf430236bb3d4622c7eb3ab36d955b956
Tomasz Plata <tomaszplata10@gmail.com>
2024-09-16 14:46:24 +0200

    Refactor: - Rewritten VisualComponent3D animation management, with use of State pattern
    Fixed:
    - Death animation works properly

--- src/shared/entities/components/visual/visual_component_3d.cpp
+++ src/shared/entities/components/visual/visual_component_3d.cpp
@@ -26,6 +26,13 @@ void VisualComponent3D::_notification(int p_notification) {
 
 			ERR_FAIL_NULL(animation_player);
 			animation_player->connect("animation_finished", callable_mp(this, &VisualComponent3D::_on_animation_finish));
+
+			DamageableComponent *damageable = ent->get_damageable_component();
+			if(damageable) {
+				damageable->connect("death", callable_mp(this, &VisualComponent3D::_on_entity_death));
+			}
+
+			animation_state = new IdleAnimationState(this);
 		}
 		break;
 
@@ -35,51 +42,36 @@ void VisualComponent3D::_notification(int p_notification) {
 }
 
 void VisualComponent3D::_on_movement_start() {
-	play_walk_animation();
+	animation_state = animation_state->on_start_walking();
+	animation_state->play_animation();
 }
 
 void VisualComponent3D::_on_movement_finish() {
-	play_idle_animation();
+	animation_state = animation_state->on_stop_walking();
+	animation_state->play_animation();
 }
 
 Entity *VisualComponent3D::get_owning_entity() {
     return Object::cast_to<Entity>(get_parent());
 }
 
-void VisualComponent3D::switch_to_idle_animation() {
+void VisualComponent3D::play_idle_animation() {
 	ERR_FAIL_NULL(animation_player);
-
-	animation_player->set_next_from_any_library("idle");
-	animation_state = AnimationState::IDLE;
+	animation_player->stop();
+	animation_player->play(get_idle_animation_name());
+	animation_player->advance(0);
 }
 
-void VisualComponent3D::switch_to_walk_animation() {
+void VisualComponent3D::play_walk_animation() {
 	ERR_FAIL_NULL(animation_player);
-
-	animation_player->set_next_from_any_library("Walk002");
-	animation_state = AnimationState::WALK;
+	animation_player->stop();
+	animation_player->play(get_walk_animation_name());
+	animation_player->advance(0);
 }
 
 void VisualComponent3D::_on_animation_finish(const String& animation_name) {
-	// TODO: Reimplement with state pattern
-	ERR_FAIL_NULL(animation_player);
-
-	// This also triggers if animation is changed
-	if((animation_player->get_current_animation_length() - animation_player->get_current_animation_position()) > 0) return; //fix
-
-	Entity *ent = get_owning_entity();
-	ERR_FAIL_NULL(ent);
-
-	MovementComponent *movement_component = ent->get_movement_component();
-
-	DamageableComponent *damageable_component = ent->get_damageable_component();
-	if(damageable_component && damageable_component->is_dead()) return; // Prevent playing new animation if entity is dead
-
-	if(movement_component && movement_component->is_currently_moving()) {
-		play_walk_animation();
-	} else {
-		play_idle_animation();
-	}
+	animation_state = animation_state->on_animation_finish();
+	animation_state->play_animation();
 }
 
 bool VisualComponent3D::is_currently_playing_death_animation() {
@@ -91,45 +83,11 @@ void VisualComponent3D::play_death_animation() {
 	animation_player->stop();
 	animation_player->play(get_death_animation_name());
 	animation_player->advance(0);
-	animation_state = AnimationState::DEAD;
 }
 
 void VisualComponent3D::_on_entity_death(const Ref<DamageObject>& damage_object) {
-	play_death_animation();
-}
-
-void VisualComponent3D::_on_owner_movement_state_change(bool is_moving) {
-	ERR_FAIL_NULL(animation_player);
-	if(get_walk_animation_name() == animation_player->get_current_animation() || get_idle_animation_name() == animation_player->get_current_animation()) return;
-
-	Entity *ent = get_owning_entity();
-	ERR_FAIL_NULL(ent);
-
-	MovementComponent *movement_component = ent->get_movement_component();
-	ERR_FAIL_NULL(movement_component);
-	
-	animation_player->stop();
-	if(movement_component->is_currently_moving()) {
-		play_walk_animation();
-	} else {
-		play_idle_animation();
-	}
-}
-
-void VisualComponent3D::play_idle_animation() {
-	ERR_FAIL_NULL(animation_player);
-	animation_player->stop();
-	animation_player->play(get_idle_animation_name());
-	animation_player->advance(0);
-	animation_state = AnimationState::IDLE;
-}
-
-void VisualComponent3D::play_walk_animation() {
-	ERR_FAIL_NULL(animation_player);
-	animation_player->stop();
-	animation_player->play(get_walk_animation_name());
-	animation_player->advance(0);
-	animation_state = AnimationState::WALK;
+	animation_state = animation_state->on_death();
+	animation_state->play_animation();
 }
 
 void VisualComponent3D::set_idle_animation_name(const StringName& new_idle_animation_name) {
@@ -150,7 +108,7 @@ StringName VisualComponent3D::get_walk_animation_name() {
 
 //Maybe use different replicator for components and status effects?
 void VisualComponent3D::_init() {
-
+	
 }
 
 void VisualComponent3D::set_death_animation_name(const StringName& new_death_animation_name) {
@@ -180,11 +138,12 @@ void VisualComponent3D::_bind_methods() {
 }
 
 void VisualComponent3D::play_animation_override(const String& animation_name) {
-	ERR_FAIL_NULL_MSG(animation_player, "VisualComponent cannot play animation - Missing AnimationPlayer");
+	animation_state = animation_state->on_animation_override(animation_name);
+	animation_state->play_animation();
+}
 
-	animation_player->stop();
+void VisualComponent3D::play_named_animation(const StringName& animation_name) {
 	animation_player->play_animation_from_any_library(animation_name);
-	animation_state = AnimationState::OVERRIDE;
 }
 
 void VisualComponent3D::set_animation_player(AnimationPlayer *new_animation_player) {
