36c96a7cd0b927550b3268d8fca50c3272d70cfe
Tomasz Plata <tomaszplata10@gmail.com>
2024-10-23 16:43:56 +0200

    - Orc's Leap slam VFX plays properly - But currently uses placeholder  - Properly deals damage in area after 0.5s

--- src/shared/abilities/orc/orc_leap_slam_ability.cpp
+++ src/shared/abilities/orc/orc_leap_slam_ability.cpp
@@ -14,4 +14,16 @@ void OrcLeapSlamAbility::_bind_methods() {
     ClassDB::bind_method(D_METHOD("get_leap_slam_area"), &OrcLeapSlamAbility::get_leap_slam_area);
     ClassDB::bind_method(D_METHOD("set_leap_slam_area", "leap_slam_area"), &OrcLeapSlamAbility::set_leap_slam_area);
     ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "leap_slam_area", PROPERTY_HINT_NODE_TYPE, Area3D::get_class_static()), "set_leap_slam_area", "get_leap_slam_area");
+
+    ClassDB::bind_method(D_METHOD("get_slam_vfx"), &OrcLeapSlamAbility::get_slam_vfx);
+    ClassDB::bind_method(D_METHOD("set_slam_vfx", "slam_vfx"), &OrcLeapSlamAbility::set_slam_vfx);
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "slam_vfx", PROPERTY_HINT_RESOURCE_TYPE, PackedScene::get_class_static()), "set_slam_vfx", "get_slam_vfx");
+}
+
+void OrcLeapSlamAbility::set_slam_vfx(const Ref<PackedScene> &new_slam_vfx) {
+    slam_vfx = new_slam_vfx;
+}
+
+Ref<PackedScene> OrcLeapSlamAbility::get_slam_vfx() {
+    return slam_vfx;
 }
--- src/shared/abilities/orc/orc_leap_slam_ability.h
+++ src/shared/abilities/orc/orc_leap_slam_ability.h
@@ -10,6 +10,7 @@ class OrcLeapSlamAbility : public Ability {
 GDCLASS(OrcLeapSlamAbility, Ability);
 private:
     Area3D *leap_slam_area = nullptr;
+    Ref<PackedScene> slam_vfx;
 
     void _slam(const Ref<ActionContext>& action_context, const Vector3 &slam_position);
 protected:
@@ -18,6 +19,9 @@ protected:
 public:
     void set_leap_slam_area(Area3D *new_leap_slam_area);
     Area3D *get_leap_slam_area();
+
+    void set_slam_vfx(const Ref<PackedScene> &new_slam_vfx);
+    Ref<PackedScene> get_slam_vfx();
 };
 
 #endif // ORC_LEAP_SLAM_ABILITY_INCLUDED
